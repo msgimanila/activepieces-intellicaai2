ed9b02cfd45930239a6fbf4bc296719186e9d039
Mario Garcia <msgimanila@yahoo.com>
2026-06-13 07:00:14 +0800

    Update language-toggle.tsx

--- packages/web/src/app/components/account-settings/language-toggle.tsx
+++ packages/web/src/app/components/account-settings/language-toggle.tsx
@@ -116,9 +116,9 @@ export const LanguageToggle = () => {
             className="text-xs text-primary hover:underline font-medium"
             rel="noopener noreferrer"
             target="_blank"
-            to="https://www.activepieces.com/docs/about/i18n"
+            to="https://www.Intellicaai.com/docs/about/i18n"
           >
-            {t('Help translate Activepieces →')}
+            {t('Help translate Intellicaai →')}
           </Link>
         </div>
       )}
