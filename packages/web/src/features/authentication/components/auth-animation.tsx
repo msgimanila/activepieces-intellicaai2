1189921dd2097603b00b0876f147f026ae5895fa
Mario Garcia <msgimanila@yahoo.com>
2026-06-13 04:29:14 +0800

    Update auth-animation.tsx

--- packages/web/src/features/authentication/components/auth-animation.tsx
+++ packages/web/src/features/authentication/components/auth-animation.tsx
@@ -1191,7 +1191,7 @@ function AuthAnimation() {
         style={{
           height: '100%',
           backgroundImage:
-            'url(https://cdn.activepieces.com/assets/auth-anim-bg.webp)',
+            'url(https://images.unsplash.com/photo-1619262105903-c65a6396c72f?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
         }}
       >
         {/* Scene Title - top center */}
