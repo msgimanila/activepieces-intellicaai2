f5b00c8246e8a7150a39fbca4c263cc3fc82b20b
Mario Garcia <msgimanila@yahoo.com>
2026-06-13 11:22:38 +0800

    Update custompage.tsx

--- packages/web/src/modules/custom-pages/custompage.tsx
+++ packages/web/src/modules/custom-pages/custompage.tsx
@@ -1,1 +1,7 @@
-//1
+import { createFileRoute } from '@tanstack/react-router'
+import CustomPageModule from '@/modules/custom-pages/custompage'
+
+export const Route = createFileRoute('/custompage')({
+  component: CustomPageModule,
+})
+
