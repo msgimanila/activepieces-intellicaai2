159d30b097a297f1c58bb3895c4a6d47e6745f50
Mario Garcia <msgimanila@yahoo.com>
2026-06-13 11:01:21 +0800

    Delete packages/web/src/app/routes/custompage.tsx

--- packages/web/src/app/routes/custompage.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import CustomPageModule from '@/modules/custom-pages/custompage'
-
-export const Route = createFileRoute('/custompage')({
-  component: CustomPageModule,
-})
